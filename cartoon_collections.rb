b46b8a6fd02bf81d48bb1165ae82a6128112c8c3
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-16 01:51:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -39,11 +39,11 @@ end
 
 
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-                      
+                      array.detect
   
   
 end
