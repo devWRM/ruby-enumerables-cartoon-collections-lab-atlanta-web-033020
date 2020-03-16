6f1da2618c90b2e81f3395e4144579412ee89349
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-16 01:07:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ end
 def summon_captain_planet(array) # code an argument here
   # Your code here
   
-                          
+                          call = []
+                          array.map { |i| call << "#{i.capitaliz} !"}
   
   
 end
