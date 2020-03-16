772382b7da2557fe50ee979bf485e8ad1892c48f
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-16 01:13:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,10 +24,10 @@ end
 
 
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array) # code an argument here
   # Your code here
   
-  
+                          array.
   
   
   
